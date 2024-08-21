7832ff84efb09fdeea4bdb4ae93e292a57dbeb64
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-08-22 00:15:23 +0600

    testing

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,11 +1,10 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-import Login from '../Login/Login'
-
 import { useState } from 'react'
 
 import SignUp from '../signup/Signup'
 import BackButton from '../../components/Shared/Botton/BackButton'
+import Login from '../login/Login'
 
 export function Join() {
   const [defaultValue, setDefaultValue] = useState('login')
